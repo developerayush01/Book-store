6adbf098a1e7b2d8c445a4ab946c40bd98f06642
Jakie Khan <khanjakie651@gmail.com>
2026-03-04 15:08:30 +0545

    Start

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const express=require('express');
+const cors=require('cors');
+
+const app=express();
+
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('Backend');
+});
+
+const PORT= process.env.PORT;
+
+app.listen(PORT,()=>{
+    console.log("Server is running");
+})
