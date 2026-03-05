9ec89ac94137967f0c89a651dc1b0084bd4c8bf6
developerayush01 <ayushbhattarai90@gmail.com>
2026-03-05 23:08:32 +0545

    Model files

---
+++ backend/models/index.js
@@ -0,0 +1,29 @@
+const User = require("./userModel");
+const Book = require("./bookModel");
+const Order = require("./orderModel");
+const OrderItem = require("./orderItemModel");
+
+/* Relationships */
+
+// User sells books
+User.hasMany(Book, { foreignKey: "seller_id" });
+Book.belongsTo(User, { foreignKey: "seller_id" });
+
+// User places orders
+User.hasMany(Order, { foreignKey: "buyer_id" });
+Order.belongsTo(User, { foreignKey: "buyer_id" });
+
+// Order contains items
+Order.hasMany(OrderItem, { foreignKey: "order_id" });
+OrderItem.belongsTo(Order, { foreignKey: "order_id" });
+
+// Each order item references a book
+Book.hasMany(OrderItem, { foreignKey: "book_id" });
+OrderItem.belongsTo(Book, { foreignKey: "book_id" });
+
+module.exports = {
+  User,
+  Book,
+  Order,
+  OrderItem
+};
