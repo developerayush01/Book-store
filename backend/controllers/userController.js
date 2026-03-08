e8b9f1f9897b387d46f58c72a51369881014cee8
developerayush01 <ayushbhattarai909@gmail.com>
2026-03-08 21:09:25 +0545

    user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,2 +1,29 @@
 const User=("../models/userModel");
 const bcypt=require("bcrypt");
+
+const registerUser = async(req,res)=>{
+
+    try {
+        const{name,email,phone,password}=req.body;
+
+        const existingUser= await User.findOne({where:{phone}});
+        if(existingUser)
+        {
+            return res.status(400).json({message:"Phone number  already registered"});
+        }
+
+        const hashPassword=await bcrypt.hash(password,10);
+
+        const user=await User.create({
+            id: uuidv4(),
+            name,
+            email,
+            phone,
+            password:hashedPassword,
+        })
+
+    } catch (error) {
+        
+    }
+
+}
