92fb99bf59cfc24c716ce2fa88fe0568b7128367
developerayush01 <ayushbhattarai909@gmail.com>
2026-03-09 22:29:36 +0545

    loginUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -47,13 +47,13 @@ const loginUser=async(req,res)=>{
 
             if(!isMatch)
             {
-            return res.status(400).json({message:"Invalid password"});
+            return res.status(401).json({message:"Invalid password"});
             }
             if(isMatch)
             {
-                return res.status(200);
+                return res.status(200).json({message:"Log in succesful"});
             }
-            return res.status({message:"Log in succesful"})
+            
         }
     } catch (error) {
         return res.status(500).json({message:"Server error"});
