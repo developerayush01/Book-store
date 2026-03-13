fd0cf66d7a5f141d6a80aebf46eabf40133b0841
developerayush01 <ayushbhattarai909@gmail.com>
2026-03-13 22:09:34 +0545

    getProfile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -71,4 +71,24 @@ const loginUser=async(req,res)=>{
 
 }
 
-module.exports= {registerUser,loginUser};
+const getProfile=async(req,res)=>{
+    try {
+        const userId=req.user.userId;
+        const user=await User.findOne({
+            where:{id:userId},
+            attributes:{exclude:["password","createdAt","updatedAt"]}
+        });
+
+        if(!user)
+        {
+            return res.status(401).json({message:"User not found"});
+        }
+            return res.status(200).json({user});
+    }
+
+    catch (error) {
+        return res.status(500).json({message:"Server error"});
+    }
+}
+    
+module.exports= {registerUser,loginUser,getProfile};
