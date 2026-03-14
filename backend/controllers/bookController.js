4651c9698cd83d7972286c58888a53d2b2caca32
developerayush01 <ayushbhattarai909@gmail.com>
2026-03-14 17:48:37 +0545

    book

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,22 +1,48 @@
-const book=require("../models/bookModel");
+const Book=require("../models/bookModel");
 const User=require("../models/userModel");
-const seller_id=req.user.userId;
+const {Op}=require("sequelize");
 
-const addBook=(req,res)=>{
-
+const addBook=async(req,res)=>{
+    
     try {
-        
+        const seller_id=req.user.userId;
         if(!seller_id)
         {
-            return res.status(201).json({message:"Seller nor found"})
+            return res.status(401).json({message:"Seller nor found"})
+        }
+        const {title,author,price,condition,description}=req.body;
+        const normalizedTitle=title.trim().toLowerCase();
+        
+        const check=await Book.findOne({
+            where:{
+                seller_id:req.user.userId,
+            title:{[Op.iLike]:normalizedTitle}
+        }
+    });
+
+        if(check)
+        {
+            return res.status(400).json({message:"Book exists already"})
         }
 
-        
 
+        const book=await Book.create({
+            title,
+            author,
+            price,
+            condition,
+            description,
+            seller_id
+        });
+
+        return res.status(201).json({message:"Book added succesfully"});
 
     } catch (error) {
         
+        return res.status(500).json("Server error");
     }
 
 }
+
+module.exports={addBook};
 
