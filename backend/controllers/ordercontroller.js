74846a48d0be576e4c3c940264d0e514d9ef5b09
developerayush01 <ayushbhattarai909@gmail.com>
2026-03-29 22:16:44 +0545

    createorder complete

--- backend/controllers/ordercontroller.js
+++ backend/controllers/ordercontroller.js
@@ -1,4 +1,5 @@
 const Order=require('../models/orderModel');
+const OrderItem=require('../models/orderItemModel');
 const Book=require("../models/bookModel");
 
 const createOrder=async(req,res)=>{
@@ -12,6 +13,7 @@ const createOrder=async(req,res)=>{
         const {bookIds}=req.body;
 
         let totalPrice=0;
+        const books=[];
         for(const bookId of bookIds)
         {
 
@@ -26,15 +28,28 @@ const createOrder=async(req,res)=>{
                 return res.status(400).json({message:"Book is already sold"});
             }
             totalPrice+=book.price;
+            books.push(book);
         }
         const order=await Order.create({
             buyer_id:loggedId,
             total_price:totalPrice,
             status:"pending"
         });
-        
+
+        for(const book of books)
+        {
+        const orderItem=await OrderItem.create({
+            order_id:order.id,
+            book_id:book.id,
+            price:book.price
+        });
+        await book.update({ status: "Sold" });
+    }
+    return res.status(201).json({message:"Order created succesfully"});
 
     } catch (error) {
         return res.status(500).json({message:"Server error on order"});
     }
 }
+
+module.exports={createOrder};
