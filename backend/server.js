16249d8392bc2408e6d15a7b6eabbf1e8a750dd6
developerayush01 <ayushbhattarai909@gmail.com>
2026-04-04 09:34:56 +0545

    frontend initiate

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ const { connectDB } = require("./config/db"); // import your DB connection
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(cookieParser());
 connectDB();
 
