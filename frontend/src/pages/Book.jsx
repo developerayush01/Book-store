eb7b28137fe7727f08057795d96601db46c1a3eb
developerayush01 <ayushbhattarai909@gmail.com>
2026-04-06 23:03:51 +0545

    book-page

--- frontend/src/pages/Book.jsx
+++ frontend/src/pages/Book.jsx
@@ -1,4 +1,40 @@
+import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import axiosInstance from '../api/axios'
+
 function Book() {
-    return <div>Book Page</div>
+    const { id } = useParams();
+    const [book,setBook]=useState(null);
+
+    useEffect(()=>{
+        const fetchBook=async()=>{
+                try {
+                const response=await axiosInstance.get(`/api/books/${id}`);
+                setBook(response.data);
+
+            } catch (error) {
+
+            }
+            
+        }
+        fetchBook();
+    },[id]);
+
+    return (
+     <div>
+            {book ? (
+                <>
+                    <h3>{book.title}</h3>
+                    <p>{book.author}</p>
+                    <p>{book.price}</p>
+                    <p>{book.condition}</p>
+                </>
+            ) : (
+                <p></p>
+            )}
+        </div>
+);
 }
+
+
 export default Book;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,36 +1,35 @@
-import {Link} from 'react-router-dom'
-import {useState,useEffect} from 'react'
-import axiosInstance from '../api/axios'
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axiosInstance from "../api/axios";
 
 function Home() {
-    const [book,setBook]=useState([]);
-    const [error,setError]=useState("");
+  const [book, setBook] = useState([]);
+  const [error, setError] = useState("");
 
-    useEffect(()=>{
-        const fetchBooks=async()=>{
-            try {
-                const response=await axiosInstance.get("/api/books/get-all");
-                setBook(response.data.book);
-            } catch (error) {
-                setError(error.response.data.message || "Something went wrong");
-            }
-        }
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const response = await axiosInstance.get("api/books/get-all");
+        setBook(response.data.book);
+      } catch (error) {
+        setError(error.response.data.message || "Something went wrong");
+      }
+    };
     fetchBooks();
-},[]);
-    
+  }, []);
 
-    return (
+  return (
     <div>
-        <h2>All Books</h2>
-        {book.map((book) => (
-            <div key={book.id}>
-                <h3>{book.title}</h3>
-                <p>{book.author}</p>
-                <p>{book.price}</p>
-                <Link to={`/books/${book.id}`}>View Details</Link>
-            </div>
-        ))}
+      <h2>All Books</h2>
+      {book.map((book) => (
+        <div key={book.id}>
+          <h3>{book.title}</h3>
+          <p>{book.author}</p>
+          <p>{book.price}</p>
+          <Link to={`/books/${book.id}`}>View Details</Link>
+        </div>
+      ))}
     </div>
-);  
+  );
 }
 export default Home;
