0db175454a75eddc5408f5404e66feeba41133c5
developerayush01 <ayushbhattarai909@gmail.com>
2026-06-20 12:52:54 +0545

    ..

--- backend/server.js
+++ backend/server.js
@@ -43,6 +43,6 @@ app.use("/api/payment", paymentRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
