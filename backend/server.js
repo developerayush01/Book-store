1af45371650d23cb7b9b520f0cf3f46950d3acbb
developerayush01 <ayushbhattarai909@gmail.com>
2026-06-26 21:47:42 +0545

    For deployment

--- backend/server.js
+++ backend/server.js
@@ -18,16 +18,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://book-store-2p7j95cbr-book-exchange.vercel.app",
-    ],
-    credentials: true,
-  }),
-);
-
 app.use(cors({
   origin: [
     "http://localhost:5173",
